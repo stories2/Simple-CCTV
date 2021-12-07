ac1095adecbd04e3b9fd15192c2799016b0d9014
Kim HyunWoo <stories2@naver.com>
2021-12-07 21:38:56 +0900

    asdf

--- web/src/index.js
+++ web/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+const path = require('path');
+const public = path.join(__dirname, 'public');
 const port = 3000
 
 app.use('/', express.static('public'));
 app.use('/hls', express.static('hls'));
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(public, 'index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
